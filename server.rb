17cca2b24fac9bda892c8487a99a5e6601dd55f0
Egbert Veenstra <egbertveenstra@gmail.com>
2014-10-03 10:58:33 +0200

    don't use puma

--- server.rb
+++ server.rb
@@ -8,8 +8,6 @@ sinatra
 ./public-ical.rb
 ".lines.map(&:strip).each &method(:require)
 
-set :server, :puma
-
 get '/public.ics' do
   response.headers['Content-type'] = 'text/calendar; charset=utf-8'
   PublicIcal::Calendar.new.icalendar.to_ical
