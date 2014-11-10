1cce191cc5e30c60422333f41e61f2c4e7837f15
Egbert Veenstra <egbertveenstra@gmail.com>
2014-11-10 22:38:54 +0100

    add preview

--- server.rb
+++ server.rb
@@ -12,3 +12,14 @@ get '/public.ics' do
   response.headers['Content-type'] = 'text/calendar; charset=utf-8'
   PublicIcal::Calendar.new.icalendar.to_ical
 end
+
+get '/' do
+  <<HTML
+  <html>
+    <body style='margin: 0'>
+      <iframe src="https://www.google.com/calendar/embed?src=v4smchckfuqjuuu62oouvddonn9k27v0%40import.calendar.google.com&ctz=Europe/Rome" style="border: 0" width="100%" height="100%" frameborder="0" scrolling="no"></iframe>
+    </body>
+  </html>
+HTML
+end
+
