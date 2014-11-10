4749ed58b8ad048aa60a4a45aba1c00c25e55116
Egbert Veenstra <egbertveenstra@gmail.com>
2014-11-10 22:48:16 +0100

    add readme text

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-[![Deploy](https://www.herokucdn.com/deploy/button.png)](https://heroku.com/deploy)
+# what
+Events organised by [Public](http://wearepublic.nl) in a nice iCal format for adding to your Google Calendar, or what have you. See http://public-ical.herokuapp.com for a preview.
+
+# how to use with Google Calendar
+- copy this link `http://public-ical.herokuapp.com/public.ics`
+- open Google Calendar
+- click on the small down arrow next to 'Other calendars'
+- choose 'add by URL'
+- paste that damn fine URL
+- ???
+- profit!
--- server.rb
+++ server.rb
@@ -5,6 +5,7 @@ active_support
 active_support/core_ext/string/filters
 icalendar
 sinatra
+github/markup
 ./public-ical.rb
 ".lines.map(&:strip).each &method(:require)
 
@@ -16,8 +17,9 @@ end
 get '/' do
   <<HTML
   <html>
-    <body style='margin: 0'>
-      <iframe src="https://www.google.com/calendar/embed?src=v4smchckfuqjuuu62oouvddonn9k27v0%40import.calendar.google.com&ctz=Europe/Rome" style="border: 0" width="100%" height="100%" frameborder="0" scrolling="no"></iframe>
+    <body style='font-family: sans-serif'>
+      #{GitHub::Markup.render 'README.md'}
+      <iframe src="https://www.google.com/calendar/embed?src=v4smchckfuqjuuu62oouvddonn9k27v0%40import.calendar.google.com&ctz=Europe/Rome" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
     </body>
   </html>
 HTML
