3e151096dacc779885ae92b62270a3ff4792e580
Egbert Veenstra <egbertveenstra@gmail.com>
2014-11-11 12:12:07 +0100

    fix calendar

--- server.rb
+++ server.rb
@@ -19,7 +19,7 @@ get '/' do
   <html>
     <body style='font-family: sans-serif'>
       #{GitHub::Markup.render 'README.md'}
-      <iframe src="https://www.google.com/calendar/embed?src=v4smchckfuqjuuu62oouvddonn9k27v0%40import.calendar.google.com&ctz=Europe/Rome" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+      <iframe src="https://www.google.com/calendar/embed?src=kj81st4p2278cr2jndae5di79k5vpnf3%40import.calendar.google.com&ctz=Europe/Rome" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
     </body>
   </html>
 HTML
